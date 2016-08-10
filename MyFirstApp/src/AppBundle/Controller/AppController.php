1456e083ac76236cb96144a14e840f10e89f7d13
kurohai <kurohai@gmail.com>
2016-08-10 10:32:26 -0500

    all the login code in one big commit. Have fun future-Evan. Hate to be you.

--- MyFirstApp/src/AppBundle/Controller/AppController.php
+++ MyFirstApp/src/AppBundle/Controller/AppController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use AppBundle\Entity\Product;
 
+
 class AppController extends Controller
 {
     /**
@@ -99,54 +100,6 @@ class AppController extends Controller
         ));
 
     }
-
-    /**
-     * @Route("/dev/products/")
-     */
-    public function listView()
-    {
-        $logger = $this->get('logger');
-        $logger->info('I just got the logger in listView');
-        $logger->info('starting db call...');
-
-        $products = $this->getDoctrine()
-            ->getRepository('AppBundle:Product')
-            ->findAll();
-
-        if (!$products) {
-            throw $this->createNotFoundException(
-                'No products found '
-            );
-        }
-        $logger->info('db call done');
-        $logger->info('returned '.count($products).' from database');
-
-        $entityFields = $products[0]->getEntityKeys();
-        $logger->info('fields on entity: '.implode(', ', $entityFields));
-
-        $results = array();
-        foreach ($products as $product)
-        {
-            $p = array(
-                'id' => $product->getId(),
-                'name' => $product->getName(),
-                'description' => $product->getDescription(),
-                'price' => $product->getPrice(),
-            );
-            $results[] = array(
-                'product' => $p
-            );
-            $logger->info('processed result: '.implode(' ', $p));
-        }
-        $data = array(
-            'entityFields' => $entityFields,
-            'results' => $results,
-            'entityFieldsCount' => count($entityFields),
-        );
-        return $this->render('default/list-view.html.twig', $data);
-
-    }
-
 
 }
 
